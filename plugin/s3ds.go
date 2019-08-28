d801a9e3853b6807d3dcff6c25afaa8ef4e37644
Michael Muré <michael.mure@consensys.net>
2019-08-28 17:14:42 +0200

    fork the module properly

--- plugin/s3ds.go
+++ plugin/s3ds.go
@@ -3,10 +3,11 @@ package plugin
 import (
 	"fmt"
 
-	s3ds "github.com/ipfs/go-ds-s3"
 	"github.com/ipfs/go-ipfs/plugin"
 	"github.com/ipfs/go-ipfs/repo"
 	"github.com/ipfs/go-ipfs/repo/fsrepo"
+
+	s3ds "github.com/INFURA/go-ds-s3"
 )
 
 var Plugins = []plugin.Plugin{
